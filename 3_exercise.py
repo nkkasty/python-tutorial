a8b3b212fb91ba6d09cff82b243bb40143216e75
nkkasty <anastasiya.kotova2005@gmail.com>
2023-12-23 18:03:55 +0100

    update

--- 3_exercise.py
+++ 3_exercise.py
@@ -1,5 +1,10 @@
 str = "pynative"
 
+#
+str[2:6]
+str[:5]
+str[3:]
+
 #1
 for i in range(len(str)):
     if i % 2 == 0:
@@ -9,3 +14,8 @@ for i in range(len(str)):
 #2
 for i in range (0, len(str),2):
     print(str[i])
+
+
+#3 - slicing
+for pismeno in str[::2]:
+    print(pismeno)
--- 6_exercise.py
+++ 6_exercise.py
@@ -8,3 +8,6 @@ for i in range(10):
     lst.append(random.randint(0,100))
 
 lst
+
+
+lst is [10, 20,33, 46, 55]
